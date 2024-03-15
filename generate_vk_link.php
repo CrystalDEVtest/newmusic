b8412d5f7647b85e7c5ea75ce5c5cf7a2eaf37ab
CrystalDEVtest <161251881+CrystalDEVtest@users.noreply.github.com>
2024-03-15 12:43:21 +0300

    Add files via upload

---
+++ generate_vk_link.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ссылка на музыку из ВКонтакте</title>
+</head>
+<body>
+    <h1>Ссылка на музыку из ВКонтакте</h1>
+
+    <?php
+    if(isset($_POST['vk_song_url'])) {
+        $vk_song_url = $_POST['vk_song_url'];
+        
+        // Извлечение ID аудиозаписи из URL
+        $vk_song_id = preg_replace('/[^0-9]/', '', $vk_song_url);
+        
+        // Генерация ссылки на аудиозапись
+        $vk_song_link = "https://vk.com/audios{$vk_song_id}?z=audio_playlist{$vk_song_id}";
+        
+        echo "<p>Ссылка на музыку из ВКонтакте: <a href='$vk_song_link'>$vk_song_url</a></p>";
+    } else {
+        echo "<p>Ошибка: URL музыкального файла из ВКонтакте не был передан.</p>";
+    }
+    ?>
+</body>
+</html>
